633e2d4ffac9098b1487b127e567b8d2b87a3fcd
Cleiber Rodrigues <cgsrjunior@inf.ufrgs.br>
2020-12-28 14:10:43 -0300

    Adding the game to the repo

---
+++ Zombie.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+const MOVE_SPEED = 200
+onready var raycast = $RayCast2D
+var player = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_to_group("zombies")
+
+
+func _physics_process(delta):
+	if player == null:
+		return
+	var vec_to_player = player.global_position - global_position
+	vec_to_player = vec_to_player.normalized()
+	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
+	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+ 
+	if raycast.is_colliding():
+		var coll = raycast.get_collider()
+		if coll.name == "Player":
+			coll.kill()
+ 
+func kill():
+	queue_free()
+ 
+func set_player(p):
+	player = p
